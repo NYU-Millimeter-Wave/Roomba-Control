90e13c354455b641ea15f042b36a22a9068191d0
Cole Smith <css459@nyu.edu>
2016-07-05 16:26:32 -0400

    Fixed the socket server and renamed to BasicSocketServer to avoid name collision

---
+++ BasicSocketServer.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env/python
+
+import signal
+import sys
+from SimpleWebSocketServer import SimpleWebSocketServer, WebSocket
+
+class SimpleHandler(WebSocket):
+
+    def handleMessage(self):
+        print("Message Received: " + str(self.data))
+
+    def handleConnected(self):
+        print("New Connection: " + str(self.address))
+
+    def handleClose(self):
+        print("Closed Connection: " + str(self.address))
+
+def signal_handler(signal, frame):
+    print(" SIGINT received, exiting...")
+    server.close
+    sys.exit(0)
+
+if __name__ == '__main__':
+    server = SimpleWebSocketServer('127.0.0.1', 9000, SimpleHandler)
+    signal.signal(signal.SIGINT, signal_handler)
+    server.serveforever()
+    signal.pause()
